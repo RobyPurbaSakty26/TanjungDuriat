195715e62839b4975e9b87890d6adb2e9d0f0a8a
RobyPurbaSakty26 <obykao26@gmail.com>
2023-01-13 23:24:35 +0700

    fix config

--- config/config.js
+++ config/config.js
@@ -1,28 +1,3 @@
-// {
-//   "development": {
-//     "username": "postgres",
-//     "password": "oby",
-//     "database": "tanjungduriat",
-//     "host": "127.0.0.1",
-//     "dialect": "postgres"
-//   },
-//   "test": {
-//     "username": "postgres",
-//     "password": "oby",
-//     "database": "database_test",
-//     "host": "127.0.0.1",
-//     "dialect": "postgres"
-//   },
-//   "production": {
-//     "username": "postgres",
-//     "password": "oby",
-//     "database": "database_production",
-//     "host": "127.0.0.1",
-//     "dialect": "postgres"
-//   }
-// }
-
-/** Destruct environment variable to get database configuration */
 const {
   DB_USER = "postgres",
   DB_PASSWORD = "oby",
