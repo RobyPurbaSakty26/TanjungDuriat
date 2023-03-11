a69bc3469186ad9eaafa2890c482ebe10c2731bc
RobyPurbaSakty26 <obykao26@gmail.com>
2023-03-11 19:58:43 +0700

    ADD (add mutasiService)

---
+++ app/service/mutasiService.js
@@ -0,0 +1,29 @@
+const mutasiRepository = require("../repository/mutasiRepository");
+
+module.exports = {
+  async getAll() {
+    const data = await mutasiRepository.getAll();
+    const count = await mutasiRepository.count();
+
+    return {
+      data,
+      count,
+    };
+  },
+
+  getByPk(id) {
+    return mutasiRepository.getByPk(id);
+  },
+
+  create(body) {
+    return mutasiRepository.create(body);
+  },
+
+  update(id, body) {
+    return mutasiRepository.update(id, body);
+  },
+
+  delete(id) {
+    return mutasiRepository.delete(id);
+  },
+};
